74e598b1a6c075848ef4d6bf52b25c8e7de41aca
mk <mkucaj86@gmail.com>
2016-04-19 23:49:33 +0200

    first init commit

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('pro')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
